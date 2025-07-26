4f49fd16e0f8675428dccfd31d6645879d633f3b
TahaLazreg <tlazreg1@gmail.com>
2025-07-26 15:14:15 -0400

    Added dialogue

---
+++ GameFiles/Dialogue/text_box.gd
@@ -0,0 +1,83 @@
+extends CanvasLayer
+class_name DialogBox
+
+const BASE_READ_RATE = 0.04;
+
+@onready var name_box = $TextboxContainer/Panel/Textbox/HBoxContainer/VBoxContainer/Name
+@onready var text_box: Label = $TextboxContainer/Panel/Textbox/HBoxContainer/DialogBox
+@onready var end_box = $TextboxContainer/Panel/Textbox/HBoxContainer/EndText
+
+@onready var container = $TextboxContainer
+
+var tween: Tween = null;
+var elapsed_tween_time = 0;
+
+var text_q = [];
+var ppl_q = [];
+
+enum state {
+	READY,
+	READING,
+	DONE
+}
+
+var currState : state = state.READY;
+
+func _ready():
+	tween = create_tween();
+	tween.connect("finished", on_tween_done);
+	
+	enqueue_text_name("There is no escape.", "Hades")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		match currState:
+			state.READING:
+				text_box.visible_ratio = 1.0;
+				tween.stop();
+				on_tween_done();
+			state.DONE:
+				hide_textbox();
+				change_to_new_state(state.READY);
+
+func _process(delta: float) -> void:
+	if (currState == state.READY and !text_q.is_empty()):
+		set_text()
+		display_textbox()
+
+func hide_textbox():
+	name_box.visible = false;
+	text_box.visible = false;
+	end_box.visible = false;
+	container.hide();
+
+func display_textbox():
+	name_box.visible = true;
+	text_box.visible = true;
+	container.show();
+	text_box.visible_ratio = 0.0;
+	
+	tween.kill();
+	tween = create_tween();
+	tween.connect("finished", on_tween_done);
+	tween.tween_property(text_box, "visible_ratio", 1.0, len(text_box.text) * BASE_READ_RATE);
+	
+	change_to_new_state(state.READING);
+
+func set_text():
+	var txt = text_q.pop_front();
+	var n_name = ppl_q.pop_front();
+	
+	name_box.text = n_name;
+	text_box.text = txt;
+
+func on_tween_done():
+	end_box.visible = true;
+	change_to_new_state(state.DONE);
+
+func change_to_new_state(new_state):
+	currState = new_state;
+
+func enqueue_text_name(text: String, name: String):
+	text_q.append(text);
+	ppl_q.append(name);
